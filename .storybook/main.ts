020c5e8ed06530090a2b41842c2e7d8d7f453e35
mkosir <kosir.mar@gmail.com>
2024-11-01 22:28:43 +0100

    chore: upgrade Storybook to v8

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,25 +1,43 @@
 import path from 'path';
 
-import { StorybookConfig } from '@storybook/core-common';
+import { StorybookConfig } from '@storybook/react-webpack5';
 
 const storybookConfig: StorybookConfig = {
   stories: ['../stories/**/*.stories.tsx'],
-  typescript: { reactDocgen: 'react-docgen-typescript' },
-  core: {
-    builder: 'webpack5',
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
   },
-  webpackFinal: (config) => {
-    config.module?.rules?.push({
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../'),
-    });
 
+  webpackFinal: (config) => {
     // Resolve absolute imports
     config.resolve?.modules?.push(path.resolve(process.cwd(), 'src'));
 
     return config;
   },
+
+  addons: ['@storybook/addon-webpack5-compiler-swc'],
 };
 
-module.exports = storybookConfig;
+// eslint-disable-next-line import/no-default-export
+export default storybookConfig;
+
+// const storybookConfig: StorybookConfig = {
+//   stories: ['../stories/**/*.stories.tsx'],
+//   typescript: { reactDocgen: 'react-docgen-typescript' },
+//   core: {
+//     builder: 'webpack5',
+//   },
+//   webpackFinal: (config) => {
+//     config.module?.rules?.push({
+//       test: /\.scss$/,
+//       use: ['style-loader', 'css-loader', 'sass-loader'],
+//       include: path.resolve(__dirname, '../'),
+//     });
+
+//     // Resolve absolute imports
+//     config.resolve?.modules?.push(path.resolve(process.cwd(), 'src'));
+
+//     return config;
+//   },
+// };
