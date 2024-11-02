f1def583d4f521b44ec98ab24c95750103c3b615
mkosir <kosir.mar@gmail.com>
2024-11-02 19:23:43 +0100

    fix(deps): update dependencies

--- stories/Counter/Counter.tsx
+++ stories/Counter/Counter.tsx
@@ -6,10 +6,14 @@ const Counter = () => {
   const onCountInc = () => setCount(count + 1);
 
   const msg = `Button clicked ${count.toString()} ${count > 1 ? 'times' : 'time'}`;
+  const marginBottom = 10;
 
   return (
     <>
-      <button style={{ fontSize: 16, padding: '5px 15px', marginBottom: '10px' }} onClick={onCountInc}>
+      <button
+        style={{ fontSize: 16, padding: '5px 15px', marginBottom: `${marginBottom.toString()}px` }}
+        onClick={onCountInc}
+      >
         Click
       </button>
       <div>{msg}</div>
